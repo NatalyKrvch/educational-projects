c6624ec1fa401ad49d3a8ea45c981780beadc5c2
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2023-03-11 23:17:31 +0200

    Update counter.jsx

--- React/First project/my-app/src/components/counter.jsx
+++ React/First project/my-app/src/components/counter.jsx
@@ -22,8 +22,10 @@ const Counter = () => {
     setCount(count - 1);
   };
 
-  const handleTagChange = () => {
-    setTags(["tag4", "tag5"]);
+  const handleTagChange = (id) => {
+    setTags((prevState) => prevState.filter((tag) => tag !== id));
+    console.log(id);
+    // setTags(["tag4", "tag5"]);
   };
 
   return (
@@ -33,7 +35,7 @@ const Counter = () => {
           <li
             key={tag}
             className="btn btn-primary btn-sm m-2"
-            onClick={handleTagChange}
+            onClick={() => handleTagChange(tag)}
           >
             {tag}
           </li>
